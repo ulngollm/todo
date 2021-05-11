ec5c8a4af24f66f62288199b249b195888ee4e67
ulngollm <ul@noknok.ru>
2021-05-11 13:19:09 +0300

    complete add new item

--- src/List.svelte
+++ src/List.svelte
@@ -2,14 +2,14 @@
     export let listItems;
 	import Checkbox from './Checkbox.svelte';
     function addNewItem(event){
-        console.log('newItem');
-        event.currentTarget.value;
-        listItems.push(event.currentTarget.value);
+        console.log(event.currentTarget.value);
+        let newItem = event.currentTarget.value;
+        listItems = [...listItems, {text:newItem}];
+        event.currentTarget.value = '';
     }
 
 </script>
     {#each listItems as item}
         <Checkbox text={item.text} completed={item.completed}/>
     {/each}
-    <Checkbox on:click={addNewItem}/>
-    <!-- для добавления нового элемента отдельный чекбокс -->
+    <input type="text" on:change={addNewItem} placeholder="Новая задача...">
